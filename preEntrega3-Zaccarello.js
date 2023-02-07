df487e801794bb302a3c6556fcf7afbd2c996f1f
Daniela Zaccarello <danizaccarello@gmail.com>
2023-02-07 00:29:10 -0300

    tercera pre-enrega

--- preEntrega3-Zaccarello.js
+++ preEntrega3-Zaccarello.js
@@ -1,108 +1,79 @@
 
 let carrito = [];
 
-let btn_carrito = document.getElementById("mostrar_carrito");
-btn_carrito.addEventListener("click", function(){
-    let carrito = document.getElementById("carrito");
-    
-    if(carrito.style.display != "none"){
-        carrito.style.display = "none";
-    }
-    else{
-        carrito.style.display = "block";
-    }
-})
-
-function agregar_carrito(e){
-    let plan = e.target.parentNode.querySelector("h5").textContent;
-    let precio = e.target.parentNode.parentNode.querySelector("span").textContent;
-    let cantidad = document.getElementById("cantidad").value;
-    let descripcion = e.target.parentNode.querySelector("p").textContent;
-    let total = 2500 * cantidad;
-
-    let consulta = {
-        plan_consulta: plan,
-        precio_plan: precio,
-        cantidad_plan: cantidad,
-        descripcion_plan: descripcion,
-        precio_total: total
+function agregar_producto(e){
+    let nombre_producto = e.target.parentNode.querySelector("h5").textContent;
+    let precio_producto = e.target.parentNode.querySelector("span").textContent;
+    let img_producto = e.target.parentNode.parentNode.querySelector("img").src;
+    let producto = {
+        nombre: nombre_producto,
+        precio: precio_producto,
+        img: img_producto,
+        cantidad:1
     };
-
-    mostrar_carrito(consulta);
+    agregar_carrito(producto);
 }
-
-
-function set_data(){
-    let plan_consulta = document.getElementById("plan_a");
-    console.log(plan_consulta);
-
-    let cantidad_plan = document.getElementById("cantidad");
-    let compra = {plan:plan_consulta.innerText, cantidad:cantidad_plan.value};
-    carrito.push(compra);
-
-    let carrito_json = JSON.stringify(carrito);
-    localStorage.setItem("carrito", carrito_json);
-    let recuperando_carrito = localStorage.getItem("carrito");
-    console.log(recuperando_carrito);
-}
-
-function mostrar_carrito(consulta){
-    let fila = document.createElement("tr");
-    fila.innerHTML = `<td>${consulta.plan_consulta}</td>
-                        <td>${consulta.precio_plan}</td>
-                        <td>${consulta.cantidad_plan}</td>
-                        <td>${consulta.descripcion_plan}</td>
-                        <td>${consulta.precio_total}</td>
-                        
-                        <td><button class="btn btn-danger borrar_elemento">Borrar</button></td>
-                        `;
-    let tabla = document.getElementById("tbody");
-    tabla.append(fila);
-
-    let btn_borrar = document.querySelectorAll(".borrar_elemento");
-    function borrar_plan(e){
-        e.target.parentNode.parentNode.remove();
-    }
-    for(let boton of btn_borrar){
-        boton.addEventListener("click", borrar_plan)
-    }
-
-    let btn_agregar = document.querySelectorAll(".agregar_elemento");
-    function agregar_elemento(e){
-        e.target.parentNode.parentNode
-    }
-}
-
-function recuperar_carrito(){
-    let plan_consulta = document.getElementById("plan_a");
-    let cantidad_plan = document.getElementById("cantidad");
-    let recuperando_carrito = localStorage.getItem("carrito");
-    recuperando_carrito = JSON.parse(recuperando_carrito);
-
-    let compra = {plan:plan_consulta.innerText, cantidad:cantidad_plan.value};
-
-    let fila = document.createElement("tr");
-    fila.innerHTML = `<h1>Traigo ${compra.plan}</h1>`;
-    let tabla = document.getElementById("tbody");
-    tabla.append(fila);
-}
-
-
-
-
-let btn_compras = document.getElementById("btn_compras");
-
-btn_compras.addEventListener("click", set_data);
-
-let btn_comprasA = document.getElementById("btn_comprasA");
-
-btn_comprasA.addEventListener("click", recuperar_carrito);
-
-
-
-
 
 let btn_compra = document.querySelectorAll(".botonCompra");
 for(let boton of btn_compra){
-    boton.addEventListener("click", agregar_carrito);
+    boton.addEventListener("click", agregar_producto);
+}
+
+function agregar_carrito(producto){
+    
+    let cantidad_producto = tbody.getElementsByClassName("cantidad_producto");
+    for(let i = 0; i < carrito.length; i++){
+        if(carrito[i].nombre == producto.nombre){
+            carrito[i].cantidad++;
+            set_data();
+            let valor_cantidad = cantidad_producto[i];
+            valor_cantidad.value++;
+            return null
+            mostrar_carrito();
+        }  
+    }
+    carrito.push(producto);
+    set_data();
+    mostrar_carrito(producto);    
+}
+
+function set_data(){
+    let carrito_json = JSON.stringify(carrito);
+    localStorage.setItem("carrito", carrito_json);
+}
+
+function local_storage(producto){
+    let recuperando_carrito = localStorage.getItem("carrito");
+    recuperando_carrito = JSON.parse(recuperando_carrito);
+    if(recuperando_carrito){
+        carrito = recuperando_carrito;
+        mostrar_carrito(producto);
+    }
+}
+
+local_storage();
+
+function mostrar_carrito(producto){
+    tbody.innerHTML = ''
+    carrito.map(producto => {
+    let fila = document.createElement("tr");
+    fila.innerHTML = `
+                      <td>${producto.nombre}</td>
+                      <td>
+                        <input type="number" value=${producto.cantidad} class="cantidad_producto"
+                      </td>
+                      <td>${producto.precio}</td>
+                      <td><button class="btn btn-danger borrar_elemento">Borrar</button></td>
+                      `;
+    let tabla = document.getElementById("tbody");
+    tabla.append(fila);
+    let btn_borrar = document.querySelectorAll(".borrar_elemento");
+    for(let boton of btn_borrar){
+        boton.addEventListener("click", borrar_producto);
+    }
+    });
+}
+
+function borrar_producto(e){
+    e.target.parentNode.parentNode.remove();
 }
